8f2a3ddf21f038830ec82351197f295a9ca2172d
RamFarid <workprojects22@gmail.com>
2023-03-06 02:48:52 +0200

    Restart sign in functionallities to test environment variables in deploy

--- src/components/signin/FacebookSignIn.jsx
+++ src/components/signin/FacebookSignIn.jsx
@@ -1,32 +1,32 @@
 import React from 'react'
 import { BsFacebook } from 'react-icons/bs'
 import Ripples from 'react-ripples'
-import { Slide, toast } from 'react-toastify'
+import { getAdditionalUserInfo, signInWithPopup } from 'firebase/auth'
+import { auth, facebookProvider } from '../../firebase.config'
 // import { useUser } from '../../contexts/UserContext'
 
 function FacebookSignIn({ clicked, setClicked }) {
   // const { displayError } = useUser()
   const facebookSigninHandler = () => {
-    toast('The game is undergoing updates now, contact us for more details', {
-      autoClose: 20000,
-      hideProgressBar: true,
-      transition: Slide,
-      toastId: 'improve',
-    })
-    return
-    // setClicked(true)
-    // signInWithPopup(auth, facebookProvider)
-    //   .then((user) => {
-    //     if (getAdditionalUserInfo(user).isNewUser) {
-    //       initNewUser(user)
-    //     }
-    //     setClicked(false)
-    //   })
-    //   .catch((er) => {
-    //     console.log(er.message)
-    //     setClicked(false)
-    //     // displayError(er.code)
-    //   })
+    // toast('The game is undergoing updates now, contact us for more details', {
+    //   autoClose: 20000,
+    //   hideProgressBar: true,
+    //   transition: Slide,
+    //   toastId: 'improve',
+    // })
+    setClicked(true)
+    signInWithPopup(auth, facebookProvider)
+      .then((user) => {
+        if (getAdditionalUserInfo(user).isNewUser) {
+          // initNewUser(user)
+        }
+        setClicked(false)
+      })
+      .catch((er) => {
+        console.log(er.message)
+        setClicked(false)
+        // displayError(er.code)
+      })
   }
   return (
     <Ripples
--- src/components/signin/GoogleSignIn.jsx
+++ src/components/signin/GoogleSignIn.jsx
@@ -1,31 +1,31 @@
+import { getAdditionalUserInfo, signInWithPopup } from 'firebase/auth'
 import React from 'react'
 import { FcGoogle } from 'react-icons/fc'
 import Ripples from 'react-ripples'
-import { Slide, toast } from 'react-toastify'
+import { auth, googleProvider } from '../../firebase.config'
 
 function GoogleSignIn({ clicked, setClicked }) {
   // const { displayError } = useUser()
   const handleGoogleAuth = () => {
-    toast('The game is undergoing updates now, contact us for more details', {
-      autoClose: 20000,
-      hideProgressBar: true,
-      transition: Slide,
-      toastId: 'improve',
-    })
-    return
-    // setClicked(true)
-    // signInWithPopup(auth, googleProvider)
-    //   .then((user) => {
-    //     if (getAdditionalUserInfo(user).isNewUser) {
-    //       initNewUser(user)
-    //     }
-    //     setClicked(false)
-    //   })
-    //   .catch((er) => {
-    //     console.log(er.code)
-    //     setClicked(false)
-    //     // displayError(er.code)
-    //   })
+    // toast('The game is undergoing updates now, contact us for more details', {
+    //   autoClose: 20000,
+    //   hideProgressBar: true,
+    //   transition: Slide,
+    //   toastId: 'improve',
+    // })
+    setClicked(true)
+    signInWithPopup(auth, googleProvider)
+      .then((user) => {
+        if (getAdditionalUserInfo(user).isNewUser) {
+          // initNewUser(user)
+        }
+        setClicked(false)
+      })
+      .catch((er) => {
+        console.log(er.code)
+        setClicked(false)
+        // displayError(er.code)
+      })
   }
   return (
     <Ripples
